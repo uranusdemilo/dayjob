c0eb688fe2b2f169f62fe3be1b4d3d4139c62bb4
Mike Wyatt <uranusdemilo@gmail.com>
2016-01-20 00:19:19 -0800

    fix external function file, added authentication to management page

---
+++ admin/admin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"> 
+<link rel="stylesheet" type="text/css" href="../css/fbstyle.css" />
+<link rel="stylesheet" type="text/css" href="../css/fbmenu.css" />
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="../scripts/lib.js"></script>
+<style>
+   .describeheader {
+      font-weight:bold;font-size:20px;
+      padding-left:20px;padding-bottom:15px;padding-top:10px;
+      font-face:imapct;
+   }
+   #sidebar{
+      float:left;
+      width:160px;
+      background-color:lightgray;
+      padding:none;
+      }
+</style>
+</head>
+<body>
+<div id="headerGraphic" style="background-color:#7babda"></div>
+<div id="headerMenu" style="menu"></div>
+<div class = "row">
+   <div class="col-sm-2" id = "sidebar" style="background-color:lightgray"></div>
+   <div class="col-sm-10"></div>
+</div>
+<div id="footer"></div>
+
+<script>
+document.getElementById("headerGraphic").innerHTML="<img src='../images/fbheadersvc.jpg'>";
+document.getElementById("headerMenu").innerHTML=menuBar('/dayjob');
+document.getElementById("sidebar").innerHTML=sideBar('/dayjob');
+document.getElementById("footer").innerHTML=jsfooter();
+</script>
+</body>
+</html>
--- scripts/lib.js
+++ scripts/lib.js
@@ -1,19 +1,22 @@
-function menuBar(){
+function menuBar(docRoot){
+   if(docRoot == undefined){
+      docRoot = ".";
+   }
    return("<ul id='menu'>" +
-	"<li><a href='./dayjob.html'>Home</a></li>" +
+	"<li><a href='" + docRoot + "/dayjob.html'>Home</a></li>" +
 	"<li><a href='#'>Company Info</a>" +
 		"<ul>" +
-			"<li><a href='./about.php'>About</a></li>" +
-			"<li><a href='./contact.php'>Contact Info</a></li>" +
+			"<li><a href='" + docRoot + "/about.php'>About</a></li>" +
+			"<li><a href='" + docRoot + "/contact.php'>Contact Info</a></li>" +
 		"</ul>" +
 	"</li>" +
 	"<li><a href='#'>Services</a>" +
 		"<ul>" +
-			"<li><a href='http://192.168.1.236/fbnew/describe.php?service=System%20Tune%20Up%20Services&text=tuneup.txt'>Tune-Up</a></li>" +
-			"<li><a href='http://192.168.1.236/fbnew/describe.php?service=System%20Tune%20Up%20Services&text=tuneup.txt'>Troubleshooting</a></li>" +
+			"<li><a href='" + docRoot + "/describe.php?service=System%20Tune%20Up%20Services&text=tuneup.txt'>Tune-Up</a></li>" +
+			"<li><a href='" + docRoot + "/describe.php?service=System%20Tune%20Up%20Services&text=tuneup.txt'>Troubleshooting</a></li>" +
 			"<li><a href='#'>Repair</a></li>" +
-			"<li><a href='http://192.168.1.236/fbnew/describe.php?service=System%20Rebuild%20Services&text=rebuild.txt'>Rebuild</a></li>" +
-			"<li><a href='http://192.168.1.236/fbnew/describe.php?service=System%20Upgrade%20Services&text=upgrades.txt'>Upgrades</a></li>" +
+			"<li><a href='" + docRoot + "/describe.php?service=System%20Rebuild%20Services&text=rebuild.txt'>Rebuild</a></li>" +
+			"<li><a href='" + docRoot + "/describe.php?service=System%20Upgrade%20Services&text=upgrades.txt'>Upgrades</a></li>" +
 			"<li><a href='#'>Drop-in Replacements</a></li>" +
 		"</ul>" +
 	"</li>" +
@@ -33,33 +36,36 @@ function menuBar(){
 "</ul>");
 }
 
-function sideBar(){
+function sideBar(docRoot){
+   if(docRoot == undefined){
+      docRoot = ".";
+   }
 return("<p><h5 style='text-align:center'>Navigation</h5></p>" +
-      "- <sidebarlink><A HREF='./dayjob.html'>Home Page</A></sidebarlink><BR>" +
+      "- <sidebarlink><A HREF='" + docRoot + "/dayjob.html'>Home Page</A></sidebarlink><BR>" +
      "<h5 style='text-align:center'>Company Info</h5>" +
      "- <A HREF='./about.php'><sidebarlink> About Flying Bovine</sidebarlink></A><br>" +
-     "- <A HREF='./contact.php'><sidebarlink>Contact Info</sidebarlink></A>" +
+     "- <A HREF='" + docRoot + "/contact.php'><sidebarlink>Contact Info</sidebarlink></A>" +
    "<BR>" +
    "<h5 style='text-align:center'>Services</h5>" +
-      "- <A HREF='./describe.php?service=System Tune Up Services&text=tuneup.txt'>" +
+      "- <A HREF='" + docRoot + "/describe.php?service=System Tune Up Services&text=tuneup.txt'>" +
         "<sidebarlink>Tune-Up</sidebarlink></A><BR>" +
-      "- <A HREF='./describe.php?service=System Tune Up Services&text=tuneup.txt'>" +
+      "- <A HREF='" + docRoot + "/describe.php?service=System Tune Up Services&text=tuneup.txt'>" +
 	"<sidebarlink>Troubleshooting</sidebarlink></A><BR>" +
-      "- <A HREF='./describe.php?service=System Rebuild Services&text=rebuild.txt'><sidebarlink>Rebuild</sidebarlink></A><BR>" +
-      "- <A HREF='./describe.php?service=System Upgrade Services&text=upgrades.txt'><sidebarlink>Upgrades</sidebarlink></A><BR>" + 
-      "- <A HREF='./describe.php?service=Drop In Replacements&text=dropin.txt'><sidebarlink>Drop-in Replacements</sidebarlink></A><BR>" +
-   "<a href='./forsaleindex.php'><h5 style='text-align:center'>Items For Sale</h5></a>" +
-      "- <A HREF='./forsale.php?pagetitle=PCs for Sale&table=pcs'><sidebarlink>PC's For Sale</sidebarlink></A><BR>" +
-      "- <A HREF='./forsale.php?pagetitle=Hard Drives for Sale&table=hdds'><sidebarlink>Hard Drives</sidebarlink></A><BR>" +
-      "- <A HREF='./forsale.php?pagetitle=Cards For Sale&table=cards'><sidebarlink>PC Cards</sidebarlink></A><BR>" +
-      "- <A HREF='./forsale.php?pagetitle=Power Supplies for Sale&table=ps'><sidebarlink>Power Supplies</sidebarlink></A><BR>" +
-      "- <A HREF='./forsale.php?pagetitle=Monitors for Sale&table=mon'><sidebarlink>Monitors</sidebarlink></A><BR>" +
-      "- <A HREF='./forsale.php?pagetitle=Other Stuff for Sale&table=other'><sidebarlink>Other Stuff</sidebarlink></A><BR><BR>" +
+      "- <A HREF='" + docRoot + "/describe.php?service=System Rebuild Services&text=rebuild.txt'><sidebarlink>Rebuild</sidebarlink></A><BR>" +
+      "- <A HREF='" + docRoot + "/describe.php?service=System Upgrade Services&text=upgrades.txt'><sidebarlink>Upgrades</sidebarlink></A><BR>" + 
+      "- <A HREF='" + docRoot + "/describe.php?service=Drop In Replacements&text=dropin.txt'><sidebarlink>Drop-in Replacements</sidebarlink></A><BR>" +
+   "<a href='" + docRoot + "/forsaleindex.php'><h5 style='text-align:center'>Items For Sale</h5></a>" +
+      "- <A HREF='" + docRoot + "/forsale.php?pagetitle=PCs for Sale&table=pcs'><sidebarlink>PC's For Sale</sidebarlink></A><BR>" +
+      "- <A HREF='" + docRoot + "/forsale.php?pagetitle=Hard Drives for Sale&table=hdds'><sidebarlink>Hard Drives</sidebarlink></A><BR>" +
+      "- <A HREF='" + docRoot + "/forsale.php?pagetitle=Cards For Sale&table=cards'><sidebarlink>PC Cards</sidebarlink></A><BR>" +
+      "- <A HREF='" + docRoot + "/forsale.php?pagetitle=Power Supplies for Sale&table=ps'><sidebarlink>Power Supplies</sidebarlink></A><BR>" +
+      "- <A HREF='" + docRoot + "/forsale.php?pagetitle=Monitors for Sale&table=mon'><sidebarlink>Monitors</sidebarlink></A><BR>" +
+      "- <A HREF='" + docRoot + "/forsale.php?pagetitle=Other Stuff for Sale&table=other'><sidebarlink>Other Stuff</sidebarlink></A><BR><BR>" +
    "<h5 style='text-align:center'>Self-Help Links</h5>" +
-      "- <FONT SIZE=1><A HREF='./virus/virus.php'>Manual Adware + Virus Removal</A><BR>" +
+      "- <FONT SIZE=1><A HREF='" + docRoot + "/virus/virus.php'>Manual Adware + Virus Removal</A><BR>" +
    "<BR><BR>" +
-   "<DIV ALIGN='CENTER'><A HREF='./login.php'>" +
-   "<IMG SRC='./images/bikesign.gif' BORDER=0 style='padding-bottom:20px'>" +
+   "<DIV ALIGN='CENTER'><A HREF='./admin/admin.php'>" +
+   "<IMG SRC='" + docRoot + "/images/bikesign.gif' BORDER=0 style='padding-bottom:20px'>" +
    "</A></DIV>");}
    
    function jsfooter(){
